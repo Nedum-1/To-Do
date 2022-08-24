fd5ac637177aed577610aed9c8aa975dac6dc07f
Nedum-1 <103987986+Nedum-1@users.noreply.github.com>
2022-08-24 22:32:07 +0100

    Update main.js
    jss

--- main.js
+++ main.js
@@ -1,71 +1,37 @@
-let c = document.querySelector("#inputt"); 
-        let h = document.querySelector("#add");
-        let i = document.querySelector(".tasks");
-        h.addEventListener('click', () => {
-            if(!c.value){
-                alert("Please Input Task");
-                return;
-            }
-            let n = document.createElement("div")
-            n.classList.add("child")
-            let e = document.createElement("input")
-            e.type="text"
-            e.value = c.value
-            e.setAttribute("readonly", "readonly")
-            e.classList.add("list")
-            n.appendChild(e)
-            i.appendChild(n)
-            let d = document.createElement("button")
-            d.innerText="Del"
-            d.classList.add("del")
-            n.appendChild(d)
-            d.style.display="none"
-            let u = document.createElement("button")
-            u.classList.add("edit")
-            u.innerText="Edit"
-            u.style.display="none"
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>To-Do Listt</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/76acaa6079.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <header> 
+                <div class="clr">
+                    <i class="fas fa-ellipsis-v" id="opt" onclick="clearr()"></i>
+                    <span id="clear" class="fas">clear all tasks</span>   
+                </div>
+                <h1 style=color:gray;>
+                    My To-Do List
+                </h1>
+                
+                <img src="" alt="">
+            </header>
+            <input type="text" placeholder="Please Enter Task Here" id="inputt">
+            <button onclick="addTask()" id="add">
+                Add
+            </button><hr>
+            <h2>Click on task to edit...</h2>
+            <div class="tasks">
+               
+            </div>
+        </div>
+        <script src="main.js">
             
-            n.appendChild(u)
-            c.value=""
-            e.addEventListener('click', () =>{
-            if (u.style.display=="none"){
-                u.style.display="inline"
-                d.style.display="inline"
-            }
-            else {
-                u.style.display="none"
-                d.style.display="none"
-            }
-            })
-            u.addEventListener('click', () =>{
-            
-            if(u.innerText=="Edit"){
-                e.removeAttribute("readonly");
-                e.focus();
-                u.innerText="Save";
-                return;
-            }
-            if(u.innerText=="Save", e.value!=""){
-                u.innerText="Edit";
-                e.setAttribute("readonly", "readonly");
-                return;
-            }
-            if(e.value==""){
-                e.removeAttribute("readonly")
-                e.focus()
-                u.innerText="Save"
-                e.placeholder="Please input task here"
-                e.style.borderColor="red"
-                d.style.borderColor="red"
-                return;
-            }
-            })
-            d.addEventListener('click', () =>{
-                n.style.display="none"
-            })
-
-        })
-        
-        
-        
-        
+        </script>
+    </body>
+    </html>
+    
